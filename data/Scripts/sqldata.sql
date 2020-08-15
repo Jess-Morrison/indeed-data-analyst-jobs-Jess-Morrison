fee2beb28bbd5d77a97b8b7245c6806fcfc625ce
Jessica Morrison <JMO@Jessicas-MacBook-Pro.local>
2020-08-15 14:06:52 -0500

    Final

--- data/Scripts/sqldata.sql
+++ data/Scripts/sqldata.sql
@@ -1,4 +1,4 @@
-/*Stuff */
+---Indeed Data_analyst_jobs---
 --
 select count (*)
 from data_analyst_jobs
@@ -44,6 +44,68 @@ group by location
 order by avg_rating desc
 /* 6B. Nebraska has the highest avg rating */
 --
+select distinct title
+from data_analyst_jobs
+where title not like '%Data%'
+/* 7A. See above */
+--
+select count(distinct title)
+from data_analyst_jobs
+where title not like '%Data%'
+/* 7B. 111 */
+--
+select count(distinct title), location
+from data_analyst_jobs
+where title not like '%Data%' 
+and location = 'CA'
+group by location
+/* 8. 29 in CA */
+---
+Select company, round(avg(star_rating),2), location,review_count
+from data_analyst_jobs
+where review_count > 5000
+group by company, star_rating, location,review_count
+/* 9A. Run above Query */
+--
+Select company, round(avg(star_rating),2), location,review_count, count(*) as c_count
+from data_analyst_jobs
+where review_count > 5000
+group by company,star_rating, location,review_count
+/* 9a. maybe the correct answer for 9A */
+--
+Select count(*)
+from (company,round(avg(star_rating),2), location,sum(review_count), count(*) as c_count
+from data_analyst_jobs
+where review_count > 5000
+group by company,star_rating, location,review_count
+having sum(review_count) > 5000
+order by) company
+----skipped 9B but will come back to it 
+Select company, round(avg(star_rating),2) as a_sr, location,review_count,count(*) as c_count
+from data_analyst_jobs
+where review_count > 5000
+group by company, location, review_count
+order by a_sr desc, c_count desc
+/* 10. The company that has 5k reviews across all locations and
+has the highest star rating is 'Kaiser Permanente' with a
+rating of 4.20 across 13 of thier locations */ 
+----
+select distinct title
+from data_analyst_jobs
+where title like '%Analyst'
+/* 11A. Run Query */
+---
+select count(distinct title) 
+from data_analyst_jobs
+where title like '%Analyst'
+/* 11B. 320 */
+--
+select distinct title
+from data_analyst_jobs
+where title Not like '%Analyst% ' and not like '%Analytics%'
+/* 12. Could not find a way to remove both terms from the search*/
+
+
 
 
 
